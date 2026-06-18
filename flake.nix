7452eb8454a962c83687edc73e51abfc868b3a48
William Fish <william.michael.fish@gmail.com>
2026-06-18 21:15:07 +0100

    chore: wire nix pre-commit hooks
    Issue: No ticket/issue

--- flake.nix
+++ flake.nix
@@ -2,20 +2,121 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      pre-commit-hooks,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
+
+        preCommitCheck = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          configPath = ".pre-commit-config-nix.yaml";
+          default_stages = [ "pre-commit" ];
+          hooks = {
+            check-added-large-files = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-case-conflicts = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-merge-conflicts = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            check-yaml = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            deadnix = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            detect-private-keys = {
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            end-of-file-fixer = {
+              enable = true;
+              excludes = [ "^demo/demo\\.cast$" ];
+              stages = [ "pre-commit" ];
+            };
+            nixfmt-rfc-style = {
+              package = pre-commit-hooks.inputs.nixpkgs.legacyPackages.${system}.nixfmt;
+              enable = true;
+              stages = [ "pre-commit" ];
+            };
+            shellcheck = {
+              enable = true;
+              args = [ "--severity=error" ];
+              stages = [ "pre-commit" ];
+            };
+            statix = {
+              enable = true;
+              settings.ignore = [ "{.direnv,.nix,.worktrees}/**" ];
+              stages = [ "pre-commit" ];
+            };
+            trim-trailing-whitespace = {
+              enable = true;
+              excludes = [ "^demo/demo\\.cast$" ];
+              stages = [ "pre-commit" ];
+            };
+            trufflehog = {
+              enable = true;
+              entry = "${pkgs.trufflehog}/bin/trufflehog filesystem --fail";
+              stages = [ "pre-commit" ];
+            };
+          };
+        };
+
+        preCommit = pkgs.writeShellScriptBin "pre-commit" ''
+          set -euo pipefail
+
+          has_config=false
+          for arg in "$@"; do
+            case "$arg" in
+              -c|--config|--config=*)
+                has_config=true
+                ;;
+            esac
+          done
+
+          if [ "$has_config" = true ]; then
+            exec ${preCommitCheck.config.package}/bin/pre-commit "$@"
+          fi
+
+          if [ "''${1:-}" = "run" ]; then
+            shift
+            exec ${preCommitCheck.config.package}/bin/pre-commit run --config .pre-commit-config-nix.yaml "$@"
+          fi
+
+          exec ${preCommitCheck.config.package}/bin/pre-commit "$@"
+        '';
+      in
+      {
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "mux";
           version = "0.1.0";
           src = ./.;
-          nativeBuildInputs = with pkgs; [ meson ninja pkg-config ];
+          nativeBuildInputs = with pkgs; [
+            meson
+            ninja
+            pkg-config
+          ];
           buildInputs = with pkgs; [ libyaml ];
-
 
           doInstallCheck = true;
           installCheckPhase = ''
@@ -31,10 +132,22 @@
         };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            meson ninja pkg-config clang-tools
-            libyaml valgrind
-          ];
+          shellHook = ''
+            ${preCommitCheck.shellHook}
+            export PATH=${preCommit}/bin:$PATH
+          '';
+
+          buildInputs =
+            preCommitCheck.enabledPackages
+            ++ (with pkgs; [
+              meson
+              ninja
+              pkg-config
+              clang-tools
+              libyaml
+              valgrind
+            ]);
         };
-      });
+      }
+    );
 }
